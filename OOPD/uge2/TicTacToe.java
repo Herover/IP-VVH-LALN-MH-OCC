deb74df4fc870251c1863283a1fc3b6f7160b343
Herover <leon.l.a.nielsen@gmail.com>
2014-11-24 11:39:03 +0100

    TicTacToe

---
+++ OOPD/uge2/TicTacToe.java
@@ -0,0 +1,30 @@
+public class TicTacToe
+{
+    public static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void newBoard() {
+
+    }
+
+    public static void printBoard() {
+
+    }
+
+    public static char get(int row, int column) {
+
+        return ' ';
+    }
+
+    public static void set(int row, int column, char val) {
+
+    }
+
+    public static boolean isGameOver() {
+
+        return true;
+    }
+}

b5bcaf3fa724572c10732d64e258e2448ef0748f
Herover <leon.l.a.nielsen@gmail.com>
2014-11-24 12:07:00 +0100

    printBoard og newBoard

--- OOPD/uge2/TicTacToe.java
+++ OOPD/uge2/TicTacToe.java
@@ -7,11 +7,27 @@ public class TicTacToe
     }
 
     public static void newBoard() {
-
+        board = new char[3][3];
+        for(int x = 0; x < 3; x++) {
+            for(int y = 0; y < 3; y++) {
+                board[x][y] = ' ';
+            }
+        }
     }
 
     public static void printBoard() {
-
+        System.out.println(" |   |   |   | ");
+        for(int x = 0; x < 3; x++) {
+            System.out.println("-+---+---+---+-");
+            System.out.println(" |   |   |   | ");
+            for(int y = 0; y < 3; y++) {
+                System.out.print(" | " + board[x][y]);
+            }
+            System.out.println(" | ");
+            System.out.println(" |   |   |   | ");
+        }
+        System.out.println("-+---+---+---+-");
+        System.out.println(" |   |   |   | ");
     }
 
     public static char get(int row, int column) {

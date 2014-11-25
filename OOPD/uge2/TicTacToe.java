df8f1341d15e22e0fb937fd64732de2137670ef1
Herover <leon.l.a.nielsen@gmail.com>
2014-11-25 20:21:23 +0100

    Rettet fejl hvor man ikke kan afbryde program + docs + småting

--- OOPD/uge2/TicTacToe.java
+++ OOPD/uge2/TicTacToe.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * TicTacToe med AI!
+ */
 public class TicTacToe
 {
     public static char[][] board = new char[3][3];
@@ -16,6 +19,10 @@ public class TicTacToe
     private static final int PLAYER_ROBOT = 1;
     private static final int PLAYER_DRUNK_ROBOT = 2;
 
+    /**
+     * To main loops holder spillet kørende, indtil brugeren vælger at afbryde
+     * spillet
+     */
     public static void main(String[] args) {
         int[] action;
         
@@ -42,12 +49,19 @@ public class TicTacToe
                 }
             }
             System.out.println("Vil du spille igen? j/n");
-            if(scan.nextLine() == "n") {
+            //Denne linje er for at konsumere linjeskift efterladt fra nextInt
+            scan.nextLine();
+            if(scan.nextLine().startsWith("n")) {
                 break;
             }
         }
     }
 
+    /**
+     * @param symbol: hvems tur det er
+     * @returns: array med [row, col] sæt
+     * Ud fra tur og indstilling hentes en handling fra en spiller.
+     */
     public static int[] getAction(char symbol) {
         if(symbol == 'x') {
             if (playerX == PLAYER_HUMAN)
@@ -67,14 +81,20 @@ public class TicTacToe
         }
     }
 
+    /**
+     * Check om man må placere en brik på et punkt
+     */
     public static boolean isValidCoord(int x, int y) {
         return (-1 < x  && x < 3 && -1 < y  && y < 3 && board[x][y] == ' ');
     }
-
+    /**
+     * Modtag handling fra et menneske
+     * Samme opførsel som getAction()
+     */
     public static int[] getHumanAction(char symbol) {
-        System.out.println("Skriv X koordinat");
+        System.out.println("Skriv række (1-" + board.length+"-)");
         int x = scan.nextInt() - 1;
-        System.out.println("Skriv Y koordinat");
+        System.out.println("Skriv kolonne (1-"+board[0].length+")");
         int y = scan.nextInt() - 1;
         while(!isValidCoord(x, y)) {
             System.out.println("Du kan ikke sætte et " + symbol + " her!");
@@ -89,6 +109,9 @@ public class TicTacToe
         return action;
     }
 
+    /**
+     * Ryd pladen
+     */
     public static void newBoard() {
         board = new char[3][3];
         for(int x = 0; x < 3; x++) {
@@ -98,6 +121,9 @@ public class TicTacToe
         }
     }
 
+    /**
+     * Print boardets indhold til skærmen, på en fancy måde
+     */
     public static void printBoard() {
         System.out.println(" |   |   |   | ");
         for(int x = 0; x < 3; x++) {
@@ -113,19 +139,35 @@ public class TicTacToe
         System.out.println(" |   |   |   | ");
     }
 
+    /**
+     * @param row
+     * @param column
+     * @return char
+     * Retunerer hvad der er på et punkt på pladen
+     */
     public static char get(int row, int column) {
         return board[row][column];
     }
 
+    /**
+     * @param row
+     * @param column
+     * Sætter en char på pladen
+     */
     public static void set(int row, int column, char val) {
         board[row][column] = val;
     }
 
     public static boolean isGameOver()
     {
-	return isGameOver(board);
+	    return isGameOver(board);
     }
 
+    /**
+     * Undersøger om spillet kan fortsætte.
+     * Dette er ikke tilfældet hvis en rækk/kolonne/diagonal er udfyldt af en
+     * spiller, eller pladen er fyldt ud.
+     */
     public static boolean isGameOver(char[][] board) {
 	    int rowCount = board.length;
 	    int columnCount = board[0].length;
@@ -199,6 +241,9 @@ public class TicTacToe
 	    return numSymbols == 9;
     }
 
+    /**
+     * @return en kopi af pladen der gerne må ændres på.
+     */
     private static char[][] getBoardCopy() {
         char[][] copy = new char[board.length][board.length];
         for(int row = 0; row < board.length; row++) {
@@ -209,13 +254,17 @@ public class TicTacToe
         return copy;
     }
 
+    /**
+     * Opfører sig som getAction, men lader computeren udregne det optimale
+     * træk.
+     */
     public static int[] getAIAction(char playerSymbol)
     {
-	int rowCount = board.length;
-	int columnCount = board[0].length;
-	int[][] corners = new int[][] {{0, 0}, {0, 2}, {2, 0}, {2, 2}};
+	    int rowCount = board.length;
+	    int columnCount = board[0].length;
+	    int[][] corners = new int[][] {{0, 0}, {0, 2}, {2, 0}, {2, 2}};
 	
-	//Try for a winning move
+	    //Try for a winning move
         for (int row = 0; row < rowCount; row++)
         {
 		    for (int column = 0; column < columnCount; column++)
@@ -261,38 +310,42 @@ public class TicTacToe
 	    }
 
 
-	//Try to place in a corner
-	for (int cornerIndex = 0; cornerIndex < corners.length; cornerIndex++)
-	    {
-		if(board[corners[cornerIndex][0]][corners[cornerIndex][1]] == ' ')
-		    {
-			return corners[cornerIndex];
-		    }
-	    }
+	    //Try to place in a corner
+	    for (int cornerIndex = 0; cornerIndex < corners.length; cornerIndex++)
+        {
+		    if(board[corners[cornerIndex][0]][corners[cornerIndex][1]] == ' ')
+	        {
+			    return corners[cornerIndex];
+	        }
+        }
 
-	//Do random move
-	ArrayList<Integer[]> emptyPositions = new ArrayList<Integer[]>();
-	for (Integer row = 0; row < rowCount; row++)
-            {
-		for (Integer column = 0; column < columnCount; column++)
+	    //Do random move
+	    ArrayList<Integer[]> emptyPositions = new ArrayList<Integer[]>();
+	    for (Integer row = 0; row < rowCount; row++)
+        {
+		    for (Integer column = 0; column < columnCount; column++)
 		    {
 			if(board[row][column] == ' ')
 			    {
-				Integer[] move = new Integer[] {row, column};
-				emptyPositions.add(move);
+				    Integer[] move = new Integer[] {row, column};
+				    emptyPositions.add(move);
 			    }
 		    }
 	    }
-	if(emptyPositions.size() > 0)
+	    if(emptyPositions.size() > 0)
 	    {
-		Random random = new Random();
-		Integer n = random.nextInt(emptyPositions.size());
-		int[] mv = new int[] {(int) emptyPositions.get(n)[0], (int) emptyPositions.get(n)[1]};
-		return mv;
+		    Random random = new Random();
+		    Integer n = random.nextInt(emptyPositions.size());
+		    int[] mv = new int[] {(int) emptyPositions.get(n)[0], (int) emptyPositions.get(n)[1]};
+		    return mv;
 	    }
         else return null;
     }
 
+    /**
+     * Opfører sig som getAction, men lader computeren udføre et tilfældigt
+     * træk.
+     */
     public static int[] getDrunkenAIAction(char playerSymbol)
     {
         Random rand = new Random();

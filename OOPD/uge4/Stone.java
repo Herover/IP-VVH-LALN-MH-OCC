722c5ca2da5e0d483e3bcbb3f49b0fdf009d9ff2
Herover <leon.l.a.nielsen@gmail.com>
2014-12-09 19:06:29 +0100

    Javadoc

--- OOPD/uge4/Stone.java
+++ OOPD/uge4/Stone.java
@@ -1,14 +1,18 @@
 public class Stone extends Entity implements Printable {
-    
+
+    /**
+     *
+     * @return Charecter to use for visual representation
+     */
     public String representation()
     {
-	return "s";
+        return "s";
     }
 
     /**
      *
-     * @param aBoard
-     * @param aPosition
+     * @param aBoard Board this Stone exists on
+     * @param aPosition Initial position of stone
      */
     public Stone(Board aBoard, Position aPosition) {
         super(aBoard, aPosition, 0);
